004a166f3d17cac20d1fc9def9685e674ca618d2
morti747 <300117314@monboreal.ca>
2021-03-18 18:34:25 -0400

    Tutoriel complété

--- b000000000-fonction.ps1
+++ b000000000-fonction.ps1
@@ -10,12 +10,27 @@
     Derniere mise à jour: yyyy-mm-dd
 
 #>
-
 # Definition de la fonction
 function Stagiaire {
 
+    [CmdletBinding()]
+    param (
+        [String]$personneNom,
+        [Int]$personneAge
+    )
+    # message de bienvenue 
+    BEGIN {Write-Verbose "Début du script"}
+    PROCESS { "Bonjour {0} ! Tu as {1} ans." -F $personneNom, $personneAge }
+    END {Write-Verbose "Fin du script"}
+
 }
 
+
+
+
+
+
+
 # Appel de la fonction
-Stagiaire
+Stagiaire "Pascal Siakam" 26  -verbose
 
